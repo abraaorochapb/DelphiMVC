5c492b72abf99a13ec695e3d0bfe490f7409e429
abraaorochapb@gmail.com <abraaorochapb>
2024-04-10 14:30:25 -0300

    Modificação na classe de conexão ao banco

--- src/view/DelphiMVC.src.view.principal.dfm
+++ src/view/DelphiMVC.src.view.principal.dfm
@@ -11,4 +11,13 @@ object frmPrincipal: TfrmPrincipal
   Font.Name = 'Segoe UI'
   Font.Style = []
   TextHeight = 15
+  object Button1: TButton
+    Left = 432
+    Top = 344
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
 end
--- src/view/DelphiMVC.src.view.principal.pas
+++ src/view/DelphiMVC.src.view.principal.pas
@@ -4,10 +4,13 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls,
+  DelphiMVC.src.dao.conexaodb;
 
 type
   TfrmPrincipal = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +23,20 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TfrmPrincipal.Button1Click(Sender: TObject);
+var
+  lConexao: TConexaodb;
+begin
+  lConexao := TConexaodb.Create;
+  lConexao.getConn.Connected := True;
+  try
+    if lConexao.getConn.Connected then
+    ShowMessage('Conectado ao db');
+  finally
+    FreeAndNil(lConexao);
+  end;
+
+end;
 
 end.
